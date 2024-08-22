cfb809e774a7d548af6318b6240a6ff0456765c4
Aleksi Patronen <apatrone19@student.oulu.fi>
2024-08-22 13:25:03 +0300

    improving dependencies

--- R/depends.R
+++ R/depends.R
@@ -1,12 +1,26 @@
 
-#This file contains dependable packages
+#This file contains dependable shinys
 
-library("shiny", quietly = TRUE)
-library("networkD3", quietly = TRUE)
-library("tibble", quietly = TRUE)
-library("huge", quietly = TRUE)
-library("dplyr", quietly = TRUE)
-library("igraph", quietly = TRUE)
-library("qgraph", quietly = TRUE)
+
+if (!require(shiny)) install.shinys('shiny', quiet = TRUE)
+library(shiny, quietly = TRUE)
+
+if (!require(networkD3)) install.packages('networkD3', quiet = TRUE)
+library(networkD3, quietly = TRUE)
+
+if (!require(tibble)) install.packages('tibble', quiet = TRUE)
+library(tibble, quietly = TRUE)
+
+if (!require(huge)) install.packages('huge', quiet = TRUE)
+library(huge, quietly = TRUE)
+
+if (!require(dplyr)) install.packages('dplyr', quiet = TRUE)
+library(igraph, quietly = TRUE)
+
+if (!require(igraph)) install.packages('igraph', quiet = TRUE)
+library(igraph, quietly = TRUE)
+
+if (!require(qgraph)) install.packages('qgraph', quiet = TRUE)
+library(qgraph, quietly = TRUE)
 
 
