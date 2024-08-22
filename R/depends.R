f1c0a23d4aea51baacd8f18aa0fd18723ec840fa
Aleksi Patronen <apatrone19@student.oulu.fi>
2024-08-22 13:37:01 +0300

    removed quit command

--- R/depends.R
+++ R/depends.R
@@ -2,25 +2,25 @@
 #This file contains dependable shinys
 
 
-if (!require(shiny)) install.shinys('shiny', quiet = TRUE)
+if (!require(shiny)) install.shinys('shiny')
 library(shiny, quietly = TRUE)
 
-if (!require(networkD3)) install.packages('networkD3', quiet = TRUE)
+if (!require(networkD3)) install.packages('networkD3')
 library(networkD3, quietly = TRUE)
 
-if (!require(tibble)) install.packages('tibble', quiet = TRUE)
+if (!require(tibble)) install.packages('tibble')
 library(tibble, quietly = TRUE)
 
-if (!require(huge)) install.packages('huge', quiet = TRUE)
+if (!require(huge)) install.packages('huge')
 library(huge, quietly = TRUE)
 
-if (!require(dplyr)) install.packages('dplyr', quiet = TRUE)
+if (!require(dplyr)) install.packages('dplyr')
 library(igraph, quietly = TRUE)
 
-if (!require(igraph)) install.packages('igraph', quiet = TRUE)
+if (!require(igraph)) install.packages('igraph')
 library(igraph, quietly = TRUE)
 
-if (!require(qgraph)) install.packages('qgraph', quiet = TRUE)
+if (!require(qgraph)) install.packages('qgraph')
 library(qgraph, quietly = TRUE)
 
 
